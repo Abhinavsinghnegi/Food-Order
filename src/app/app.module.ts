298a748dbe34b72d88a1c8f09dc8244b2b103033
Abhinav <abhinavnegi842@gmail.com>
2022-11-23 00:42:48 +0530

    Add food details using services and component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
+import { RatingModule } from 'ng-starrating';
+import { CurrencyPipe } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,9 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    RatingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/services/food/food.service.ts
+++ src/app/services/food/food.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Foods } from 'src/app/shared/models/food';
 
 @Injectable({
   providedIn: 'root'
@@ -7,17 +8,75 @@ export class FoodService {
 
   constructor() { }
 
-  getAll(): string[]{
+  getAll():Foods[]{
     return [
-      '/assets/food images/food-1.jpg',
-      '/assets/food images/food-8.jpg',
-      '/assets/food images/food-3.jpg',
-      '/assets/food images/food-4.jpg',
-      '/assets/food images/food-5.jpg',
-      '/assets/food images/food-6.jpg',
-      '/assets/food images/food-7.jpg',
-      '/assets/food images/food-8.jpg',
-    ]
+      {
+        id : 1,
+        name: 'Pizza',
+        cookTime: '10-20',
+        price: 10,
+        favorite: false,
+        origins: ['italy'],
+        star: 4.5,
+        imageUrl : '/assets/food images/food-1.jpg',
+        tags: ['FastFood','Lunch'],
+      },
+      {
+        id : 2,
+        name: 'Pizza',
+        cookTime: '10-20',
+        price: 10,
+        favorite: false,
+        origins: ['italy'],
+        star: 4.5,
+        imageUrl : '/assets/food images/food-3.jpg',
+        tags: ['FastFood','Lunch'],
+      },
+      {
+        id : 3,
+        name: 'Pizza',
+        cookTime: '10-20',
+        price: 10,
+        favorite: false,
+        origins: ['italy'],
+        star: 4.5,
+        imageUrl : '/assets/food images/food-4.jpg',
+        tags: ['FastFood','Lunch'],
+      },
+      {
+        id : 4,
+        name: 'Pizza',
+        cookTime: '10-20',
+        price: 10,
+        favorite: false,
+        origins: ['italy'],
+        star: 4.5,
+        imageUrl : '/assets/food images/food-5.jpg',
+        tags: ['FastFood','Lunch'],
+      },
+      {
+        id : 5,
+        name: 'Pizza',
+        cookTime: '10-20',
+        price: 10,
+        favorite: false,
+        origins: ['italy'],
+        star: 4.5,
+        imageUrl : '/assets/food images/food-6.jpg',
+        tags: ['FastFood','Lunch'],
+      },
+      {
+        id : 6,
+        name: 'Pizza',
+        cookTime: '10-20',
+        price: 10,
+        favorite: false,
+        origins: ['italy'],
+        star: 4.5,
+        imageUrl : '/assets/food images/food-7.jpg',
+        tags: ['FastFood','Lunch'],
+      }
+    ];
   }
 }
 
