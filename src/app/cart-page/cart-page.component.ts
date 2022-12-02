8323fdfa2fe1d8ba802b6782b33047f66cb24a10
Abhinav <abhinavnegi842@gmail.com>
2022-12-02 23:52:12 +0530

    feat:add functionality to add to cart button

--- src/app/cart-page/cart-page.component.ts
+++ src/app/cart-page/cart-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from '../services/cart.service';
+import { CartItem } from '../shared/models/cartItem';
 
 @Component({
   selector: 'app-cart-page',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart-page.component.css']
 })
 export class CartPageComponent implements OnInit {
-
-  constructor() { }
+  cart!: Cart;
+  constructor(private cartService : CartService) {
+    this.setCart();
+   }
 
   ngOnInit(): void {
+  }
+
+  setCart(){
+    this.cart = this.cartService.getCart();
+  }
+
+  removeFromCart(cartItem: CartItem){
+    this.cartService.removeFromCart(cartItem.food.id);
+    this.setCart();
+  }
+
+  changeQuantity(cartItem:CartItem, quantityInString:string){
+    const quantity = parseInt(quantityInString);
+    this.cartService.changeQuantity(cartItem.food.id, quantity);
+    this.setCart();
   }
 
 }
